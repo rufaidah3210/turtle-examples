7751f0badd2b93f0fc65c5d663c32386e6d2862b
rufaidah3210 <rufaidah3210@gmail.com>
2023-05-14 10:28:15 +0600

    w

---
+++ example1/example1.2.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.right(45)
+turtle.forward(100)
+
+turtle.left(100)
+turtle.forward(100)
+
+turtle.right(100)
+turtle.forward(100)
+
+turtle.left(100)
+turtle.forward(100)
+turtle.done()
+
