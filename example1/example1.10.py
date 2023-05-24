d4ec34b049f2fe5fee3e019b859cf328b3c88243
rufaidah3210 <rufaidah3210@gmail.com>
2023-05-24 22:51:17 +0600

    face

---
+++ example1/example1.10.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.speed(1)
+turtle.width(1)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.fd(90)
+turtle.right(90)
+turtle.fd(90)
+turtle.left(90)
+turtle.fd(90)
+turtle.right(90)
+turtle.fd(90)
+turtle.left(90)
+turtle.fd(90)             
+turtle.right(90)
+turtle.fd(90)
+turtle.done()
